af54026806849c73f8b1cdc3ef032856da0be66e
Lisa Connell <connell.lisamarie@gmail.com>
2018-03-09 19:28:09 +0000

    automatically backed up by learn

--- other_file.js
+++ other_file.js
@@ -1,4 +1,9 @@
+<<<<<<< HEAD
 
 var index = require("./index.js")
  
 console.log(index.name)
+=======
+console.log(name)
+var index = require("./index.js")
+>>>>>>> ca9f9671fbe5fc6e5dfac0d49fd5eccf54cbef35
